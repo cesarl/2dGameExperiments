f4a6ca1e234536a995dc03c85fedbb8cf18cc459
Cesar Leblic <cesar.leblic@gmail.com>
2013-10-06 19:59:57 +0200

    Check at init if system have component required not at runtime

--- isaac2/src/manager/SystemManager.hpp
+++ isaac2/src/manager/SystemManager.hpp
@@ -13,64 +13,66 @@ class				SystemManager : public Singleton<SystemManager>
 {
 public:
 
-  void				add(System *sys);
+	void				add(System *sys);
 
-  void				update(float time, const ALLEGRO_EVENT &ev);
-  void				draw(float time, const ALLEGRO_EVENT &ev);
+	void				update(float time, const ALLEGRO_EVENT &ev);
+	void				draw(float time, const ALLEGRO_EVENT &ev);
 
-  template			<class T>
-  T				*add(int priority, bool draw = false)
-  {
-    std::multimap<const char *, System *>::iterator it;
-    T				*sys;
+	template			<class T>
+	T				    *add(int priority, bool draw = false)
+	{
+		std::multimap<const char *, System *>::iterator it;
+		T				*sys;
 
-    it = map_.find(typeid(T).name());
-    if (it != map_.end())
-      return static_cast<T*>(it->second);
-    sys = new T;
-    if (!sys)
-      throw OutOfMemory("Cannot allocate new System");
-    map_.insert(std::pair<const char *, System *>(typeid(T).name(), sys));
-    if (!draw)
-      list_.insert(std::pair<int, System *>(priority, sys));
-    else
-      drawList_.insert(std::pair<int, System*>(priority, sys));
-    sys->init();
-    return sys;
-  }
+		it = map_.find(typeid(T).name());
+		if (it != map_.end())
+			return static_cast<T*>(it->second);
+		sys = new T;
+		if (!sys)
+			throw OutOfMemory("Cannot allocate new System");
+		sys->init();
+		if (! sys->hasComponentRequired())
+			throw SystemWithoutComponentRequired("System is empty : ", typeid(*this).name());
+		map_.insert(std::pair<const char *, System *>(typeid(T).name(), sys));
+		if (!draw)
+			list_.insert(std::pair<int, System *>(priority, sys));
+		else
+			drawList_.insert(std::pair<int, System*>(priority, sys));
+		return sys;
+	}
 
-  template			<class T>
-  T				*getSystem()
-  {
-    std::multimap<const char *, System *>::iterator it;
+	template			<class T>
+	T				*getSystem()
+	{
+		std::multimap<const char *, System *>::iterator it;
 
-    it = map_.find(typeid(T).name());
-    if (it != map_.end())
-      return static_cast<T*>(it->second);
-    return NULL;
-  }
+		it = map_.find(typeid(T).name());
+		if (it != map_.end())
+			return static_cast<T*>(it->second);
+		return NULL;
+	}
 
 private:
-  std::multimap<int, System*>	list_;
-  std::multimap<const char *, System*> map_;
-  std::multimap<int, System*>	drawList_;
+	std::multimap<int, System*>	list_;
+	std::multimap<const char *, System*> map_;
+	std::multimap<int, System*>	drawList_;
 private:
-  SystemManager()
-  {}
+	SystemManager()
+	{}
 
-  virtual ~SystemManager()
-  {
-    std::multimap<int, System*>::iterator it;
+	virtual ~SystemManager()
+	{
+		std::multimap<int, System*>::iterator it;
 
-    it = list_.begin();
-    while (it != list_.end())
-      {
-	delete it->second;
-	++it;
-      }
-  }
+		it = list_.begin();
+		while (it != list_.end())
+		{
+			delete it->second;
+			++it;
+		}
+	}
 private:
-  friend class Singleton<SystemManager>;
+	friend class Singleton<SystemManager>;
 };
 
 #endif				// __SYSTEM_MANAGER_HPP__
--- isaac2/src/system/System.hpp
+++ isaac2/src/system/System.hpp
@@ -28,14 +28,15 @@ public:
 
 	bool				match(const EntityData &entity)
 	{
-		if (componentsRequired_.none())
-		{
-			throw SystemWithoutComponentRequired("System is empty : ", typeid(*this).name());
-		}
 		if ((entity.components & componentsRequired_) == componentsRequired_)
 			return true;
 		return false;
 	}
+
+bool                   hasComponentRequired()
+{
+	return (!componentsRequired_.none());
+}
 
 protected:
 	std::bitset<64>		componentsRequired_;
